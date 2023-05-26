97745310b3bece77ef0d1fff3306dd795f9f290e
Timur-Ooganbekov <ooganbekovtimur@gmail.com>
2023-05-26 19:30:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RegistrationForm extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            fullName: '',
+            phoneNumber: '',
+            email: ''
+        };
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { fullName, phoneNumber, email } = this.state;
+        const formData = {
+            fullName,
+            phoneNumber,
+            email
+        };
+        alert(JSON.stringify(formData));
+    };
+
+    handleChange  = (event) => {
+        const { name,value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    render() {
+        const { fullName, phoneNumber, email } = this.state;
+
+        return (
+            <form onSubmit={this.handleSubmit} className="registration-form">
+                <div>
+                    <input
+                        type="text"
+                        placeholder="ФИО"
+                        name="fullName"
+                        value={fullName}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="tel"
+                        placeholder="Номер телефона"
+                        name="phoneNumber"
+                        value={phoneNumber}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
 }
 
-export default App;
+export default RegistrationForm;
